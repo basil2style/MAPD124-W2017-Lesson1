e7d3a51b1d4ee901766038a2dd8607494d4ce76a
Basil <basiltalias92@gmail.com>
2017-01-10 22:04:40 -0500

    -Added commit -Removed view did load

--- MAPD124-W2017-Lesson1/ViewController.swift
+++ MAPD124-W2017-Lesson1/ViewController.swift
@@ -10,16 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var appLabel: UILabel!
+    
+    //button click method
+    @IBAction func ButtonClick(_ sender: UIButton) {
+        print(sender.currentTitle!)
+        appLabel.text = "Touched :\(sender.currentTitle!)"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    
 }
 
