565f30549935004a4dc52a2224fc14dc0684329d
Basil <basiltalias92@gmail.com>
2017-01-10 21:21:45 -0500

    Initial Commit

---
+++ MAPD124-W2017-Lesson1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MAPD124-W2017-Lesson1
+//
+//  Created by Basil on 2017-01-10.
+//  Copyright © 2017 Basil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
